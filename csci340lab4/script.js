f6aeaab7144522119eaea42b249db1d4a57a1ce5
Dylan Carter <167344346+dylcarinc@users.noreply.github.com>
2026-02-06 11:43:00 -0600

    replaced lyrics with wikipedia

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -1,4 +1,3 @@
-    var track = "";
     var artist = "";
     $.ajax({
       dataType: "jsonp",
@@ -6,20 +5,20 @@
       url: "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=thewwdc&api_key=e62caada26f13f1f30556b61299dbf83&format=json",
       success: function(results) {
         $('#success #Track').text(results.recenttracks.track[0].name);
-        track = encodeURI(results.recenttracks.track[0].name);
         $('#success #Album').text(results.recenttracks.track[0].album['#text']);
         $('#success #Artist').text(results.recenttracks.track[0].artist['#text']);
-        artist = encodeURI(results.recenttracks.track[0].artist['#text']);
+        artist = (results.recenttracks.track[0].artist['#text']).replaceAll(' ', '_');
         $('#success #Cover').html('<img src="' + results.recenttracks.track[0].image[2]['#text'] + '" />');
          $.ajax({
                 dataType: "json",
                 method: 'GET',
-                url: "https://api.lyrics.ovh/v1/" + artist + "/" + track,
+                url: "https://en.wikipedia.org/api/rest_v1/page/summary/" + artist,
                 success: function(results) {
-                    $('#success #Lyrics').text(results.lyrics);
+                    $('#success #Lyrics').text(results.extract);
+                    $('#success #Link').html(`<a href="${results.content_urls.desktop.page}">Read more on Wikipedia</a>`);
                 },
                 error: function(xhr,status,error) {
-                    console.log("https://api.lyrics.ovh/v1/" + artist + "/" + track);
+                    console.log(error);
                 }
                 });
       },
