4ddc8f1e216384a703f2b81fdd133ea4eb7895fc
Dylan Carter <167344346+dylcarinc@users.noreply.github.com>
2026-02-05 20:48:25 -0600

    last.fm API works, getting most recent track I've listened to

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -1,15 +1,14 @@
-$(document).ready(function() {
-  $('.cookie').click(function() {
     $.ajax({
       dataType: "jsonp",
       jsonpCallback: "parseQuote",
       url: "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=thewwdc&api_key=e62caada26f13f1f30556b61299dbf83&format=json",
       success: function(results) {
-        $('#Track').text(results["recenttracks.track.0.name"]);
+        $('#success #Track').text(results.recenttracks.track[0].name);
+        $('#success #Album').text(results.recenttracks.track[0].album['#text']);
+        $('#success #Artist').text(results.recenttracks.track[0].artist['#text']);
+        $('#success #Cover').html('<img src="' + results.recenttracks.track[0].image[2]['#text'] + '" />');
       },
       error: function(xhr,status,error) {
         console.log(error);
       }
     });
-  });
-});
