b349554972086e77b4bf8d2979971bfe887dee11
Dylan Carter <167344346+dylcarinc@users.noreply.github.com>
2026-02-05 21:38:34 -0600

    Lyrics work now!

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -1,12 +1,27 @@
+    var track = "";
+    var artist = "";
     $.ajax({
       dataType: "jsonp",
       jsonpCallback: "parseQuote",
       url: "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=thewwdc&api_key=e62caada26f13f1f30556b61299dbf83&format=json",
       success: function(results) {
         $('#success #Track').text(results.recenttracks.track[0].name);
+        track = encodeURI(results.recenttracks.track[0].name);
         $('#success #Album').text(results.recenttracks.track[0].album['#text']);
         $('#success #Artist').text(results.recenttracks.track[0].artist['#text']);
+        artist = encodeURI(results.recenttracks.track[0].artist['#text']);
         $('#success #Cover').html('<img src="' + results.recenttracks.track[0].image[2]['#text'] + '" />');
+         $.ajax({
+                dataType: "json",
+                method: 'GET',
+                url: "https://api.lyrics.ovh/v1/" + artist + "/" + track,
+                success: function(results) {
+                    $('#success #Lyrics').text(results.lyrics);
+                },
+                error: function(xhr,status,error) {
+                    console.log("https://api.lyrics.ovh/v1/" + artist + "/" + track);
+                }
+                });
       },
       error: function(xhr,status,error) {
         console.log(error);
