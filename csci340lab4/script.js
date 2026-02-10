2e53f1d4dbbdb169da30b0a4b156c61a814b44bd
Dylan Carter <167344346+dylcarinc@users.noreply.github.com>
2026-02-09 19:43:30 -0600

    added buttons to see 50 most recent tracks

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -1,29 +1,68 @@
-    var artist = "";
-    $.ajax({
-      dataType: "jsonp",
-      jsonpCallback: "parseQuote",
-      url: "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=thewwdc&api_key=e62caada26f13f1f30556b61299dbf83&format=json",
-      success: function(results) {
-        $('#success #Track').text(results.recenttracks.track[0].name);
-        $('#success #Album').text(results.recenttracks.track[0].album['#text']);
-        $('#success #Artist').text(results.recenttracks.track[0].artist['#text']);
-        artist = (results.recenttracks.track[0].artist['#text']).replaceAll(' ', '_');
-        $('#success #Cover').html('<img src="' + results.recenttracks.track[0].image[2]['#text'] + '" />');
-         $.ajax({
-                dataType: "json",
-                method: 'GET',
-                url: "https://en.wikipedia.org/api/rest_v1/page/summary/" + artist,
-                success: function(results) {
-                    $('#success #Wikipedia').text(results.extract);
-                    $('#success #Link').html(`<a href="${results.content_urls.desktop.page}">Read more on Wikipedia</a>`);
-                    $('#success #Picture').html('<img src="' + results.originalimage.source + '" />');
-                },
-                error: function(xhr,status,error) {
-                    console.log(error);
-                }
-                });
-      },
-      error: function(xhr,status,error) {
-        console.log(error);
+var currentIndex = 0;
+var trackList = []
+$.ajax({
+  dataType: "jsonp",
+  jsonpCallback: "parseQuote",
+  url: "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=thewwdc&api_key=e62caada26f13f1f30556b61299dbf83&format=json",
+  success: function(results) {
+    trackList = results.recenttracks.track;
+    $('#totalTracks').text(trackList.length);
+    displayTrack(0);
+  },
+  error: function(xhr,status,error) {
+    console.log(error);
+  }
+});
+function displayTrack(index) {
+  if (index < 0 || index >= trackList.length) return;
+  
+  currentIndex = index;
+  var track = trackList[index];
+  
+  $('#success #Track').text(track.name);
+  $('#success #Album').text(track.album['#text']);
+  $('#success #Artist').text(track.artist['#text']);
+  $('#success #Cover').html('<img src="' + track.image[2]['#text'] + '" />');
+  $('#currentTrack').text(index + 1);
+  
+
+  $('#success #Wikipedia').text('');
+  $('#success #Link').html('');
+  $('#success #Picture').html('');
+
+  var artist = track.artist['#text'].replaceAll(' ', '_');
+  $.ajax({
+    dataType: "json",
+    method: 'GET',
+    url: "https://en.wikipedia.org/api/rest_v1/page/summary/" + artist,
+    success: function(results) {
+      $('#success #Wikipedia').text(results.extract);
+      $('#success #Link').html(`<a href="${results.content_urls.desktop.page}" target="_blank">Read more on Wikipedia</a>`);
+      if (results.originalimage) {
+        $('#success #Picture').html('<img src="' + results.originalimage.source + '" />');
       }
-    });
+    },
+    error: function(xhr, status, error) {
+      $('#success #Wikipedia').text('Wikipedia information not available for this artist.');
+      console.log(error);
+    }
+  });
+  
+  updateButtons();
+}  
+function updateButtons() {
+  $('#prevBtn').prop('disabled', currentIndex === 0);
+  $('#nextBtn').prop('disabled', currentIndex === TrackList.length - 1);
+}
+
+$('#prevBtn').click(function() {
+  if (currentIndex > 0) {
+    displayTrack(currentIndex - 1);
+  }
+});
+
+$('#nextBtn').click(function() {
+  if (currentIndex < trackList.length) {
+    displayTrack(currentIndex + 1);
+  }
+});
